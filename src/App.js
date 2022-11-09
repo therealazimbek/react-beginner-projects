e08e70d28616ca4f90a9e208ed91cf9981539cba
therealazimbek <aaazim04012002@gmail.com>
2022-11-09 20:18:20 +0600

    user inviter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invited, setInvited] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.warn(err);
+        alert("Error fetching users!");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchInput = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invited.includes(id)) {
+      setInvited((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvited((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSend = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invited.length} />
+      ) : (
+        <Users
+          onChangeSearchInput={onChangeSearchInput}
+          searchInput={searchInput}
+          items={users}
+          isLoading={isLoading}
+          invited={invited}
+          onClickInvite={onClickInvite}
+          onClickSend={onClickSend}
+        />
+      )}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import React from "react";
+import { Skeleton } from "./Skeleton";
+import { User } from "./User";
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({
+  items,
+  isLoading,
+  searchInput,
+  onChangeSearchInput,
+  invited,
+  onClickInvite,
+  onClickSend,
+}) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          value={searchInput}
+          onChange={onChangeSearchInput}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +32,30 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items
+            .filter((obj) => {
+              const full = (obj.first_name + obj.last_name).toLowerCase();
+
+              return (
+                full.includes(searchInput.toLowerCase()) ||
+                obj.email.toLowerCase().includes(searchInput.toLowerCase())
+              );
+            })
+            .map((obj) => (
+              <User
+                onClickInvite={onClickInvite}
+                isInvited={invited.includes(obj.id)}
+                key={obj.id}
+                {...obj}
+              />
+            ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {invited.length > 0 && (
+        <button onClick={onClickSend} className="send-invite-btn">
+          Отправить приглашение
+        </button>
+      )}
     </>
   );
 };
